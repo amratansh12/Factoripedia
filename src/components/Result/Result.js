9d0a7b6c510b19a3f0d9a790e19ff51c0cedcaef
amratansh12 <ashri1205@gmail.com>
2023-05-19 19:36:06 +0530

    removing extra paras in result

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -6,7 +6,7 @@ const Result = ({type, interest, years, factor, factorCalculate,abygForUniform,p
         <div className="containerResult">
             <h2 id='resultHeading'>factor is</h2>
             <h2 id='factorValue'>{factor}</h2>
-            <p id='normalFactors'> {type} , {interest} , {years} , {incrementPercentage}</p>
+            {/* <p id='normalFactors'> {type} , {interest} , {years} , {incrementPercentage}</p> */}
             <button onClick={(selectedFactor === 'uniform') ? abygForUniform : ((selectedFactor === 'geometric') ? pbyfForGeometric : factorCalculate)}>Generate factor</button>
             <p id='uniformResult'>{(selectedFactor === 'uniform')?'Annual equivalent for the entered Uniform Gradient amount is Rs. ' + incrementAmount*factor : ((selectedFactor === 'geometric') ? 'Present worth for the entered Uniform first year end amount is Rs. ' + firstYearEndAmount*factor : null) }</p>
         </div>
